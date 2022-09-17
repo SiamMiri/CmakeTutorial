f044fc03c874de4bf5d3be5edcdc7a75a9b02336
SiamMiri <siamakmirifar@gmail.com>
2022-09-17 13:53:13 +0200

    glwf init window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(){
 
-int age;
-std::cout << "Enter your age: " << std::endl; 
-std::cin  >> age; 
-std::cout << "Your agen is: " << age  << std::endl;
+    GLFWwindow *window;
+    if( !glfwInit() )
+        {
+            fprintf( stderr, "Failed to initialize GLFW\n" );
+            exit( EXIT_FAILURE );
+        }
 
+        window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+        if (!window)
+        {
+            fprintf( stderr, "Failed to open GLFW window\n" );
+            glfwTerminate();
+            exit( EXIT_FAILURE );
+        }
+
+        // Main loop
+        while( !glfwWindowShouldClose(window) )
+        {
+            // Draw gears
+            // draw();
+
+            // Update animation
+            // animate();
+
+            // Swap buffers
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        // Terminate GLFW
+        glfwTerminate();
+
+        // Exit program
+        exit( EXIT_SUCCESS );
 }
